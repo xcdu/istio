fcc46a4a0cdc0cba90d8059cbb7f5e0798f42e7f
mekkirachedine <mekkirachedine1@gmail.com>
2021-03-04 05:07:18 -0600

    bert model complete

--- models/bert/bert_model.py
+++ models/bert/bert_model.py
@@ -11,66 +11,69 @@ from official.nlp import optimization  # to create AdamW optmizer
 import matplotlib.pyplot as plt
 
 from model_mapping import name_to_handle, model_to_preprocess
+from data_preprocessing import get_data
 
 tf.get_logger().setLevel('ERROR')
-
-# Download the IMDB dataset
-
-dataset_dir = 'Istio_data'
-
-train_dir = os.path.join(dataset_dir, 'train')
-test_dir = os.path.join(dataset_dir, 'test')
-
+data_path = "post_fixed.csv"
+bert_model_name = 'small_bert/bert_en_uncased_L-12_H-768_A-12'
 AUTOTUNE = tf.data.AUTOTUNE
 batch_size = 32
 seed = 42
 epochs = 60
 
-raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
-    directory=train_dir,
-    batch_size=batch_size,
-    validation_split=0.2,
-    subset='training',
-    seed=seed)
-
-class_names = raw_train_ds.class_names
-train_ds = raw_train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-val_ds = tf.keras.preprocessing.text_dataset_from_directory(
-    directory=train_dir,
-    batch_size=batch_size,
-    validation_split=0.2,
-    subset='validation',
-    seed=seed)
-
+train_ds, val_ds, test_ds, vocab_size = get_data(data_path, bert_model_name, batch_size, 70, 10, 20)
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-test_ds = tf.keras.preprocessing.text_dataset_from_directory(
-    test_dir,
-    batch_size=batch_size)
-
 test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 # Loading models from TensorFlow Hub
-bert_model_name = 'small_bert/bert_en_uncased_L-12_H-768_A-12'
 tfhub_handle_encoder = name_to_handle(bert_model_name)
 bert_model = hub.KerasLayer(tfhub_handle_encoder)
 tfhub_handle_preprocess = model_to_preprocess(bert_model_name)
 bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
 
-# define your model
-def build_classifier_model():
-  inputs = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-  preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
-  encoder_inputs = preprocessing_layer(inputs)
-  encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
-  outputs = encoder(encoder_inputs)
-  net = outputs['pooled_output']
-  net = tf.keras.layers.Dropout(0.1)(net)
-  net = tf.keras.layers.Dense(5, activation='softmax', name='classifier')(net)
-  return tf.keras.Model(inputs, net)
+class bert_classifier_model(tf.keras.Model):
 
-classifier_model = build_classifier_model()
+    def __init__(self, vocabulary_size, embedding_dimensions, cnn_filters, dnn_units, model_output_classes,
+                 dropout_rate, training=False, name="text_model"):
+        super(bert_classifier_model, self).__init__(name=name)
+        self.embedding = tf.keras.layers.Embedding(vocabulary_size, embedding_dimensions)
+        self.cnn_layer1 = tf.keras.layers.Conv1D(filters=cnn_filters,
+                                                 kernel_size=2,
+                                                 padding="valid",
+                                                 activation="relu")
+        self.cnn_layer2 = tf.keras.layers.Conv1D(filters=cnn_filters,
+                                                 kernel_size=3,
+                                                 padding="valid",
+                                                 activation="relu")
+        self.cnn_layer3 = tf.keras.layers.Conv1D(filters=cnn_filters,
+                                                 kernel_size=4,
+                                                 padding="valid",
+                                                 activation="relu")
+
+        self.pool = tf.keras.layers.GlobalMaxPool1D()
+
+        self.dense1 = tf.keras.layers.Dense(units=dnn_units, activation="relu")
+        self.dense2 = tf.keras.layers.Dense(units=model_output_classes, activation="softmax")
+        self.dropout = tf.keras.layers.Dropout(rate=dropout_rate)
+
+    def call(self, inputs, training):
+        layer0 = self.embedding(inputs)
+        layer1 = self.cnn_layer1(layer0)
+        layer1 = self.pool(layer1)
+        layer2 = self.cnn_layer2(layer0)
+        layer2 = self.pool(layer2)
+        layer3 = self.cnn_layer3(layer0)
+        layer3 = self.pool(layer3)
+
+        all_layers = tf.concat([layer1, layer2, layer3], axis=-1)
+        all_layers = self.dense1(all_layers)
+        all_layers = self.dropout(all_layers, training)
+        all_layers = self.dense2(all_layers)
+        return all_layers
+
+classifier_model = bert_classifier_model(vocabulary_size=vocab_size, embedding_dimensions=200, cnn_filters=100,
+                                         dnn_units=256, model_output_classes=5, dropout_rate=0.2)
 
 classifier_model.compile(
     optimizer=tf.keras.optimizers.RMSprop(),  # Optimizer
@@ -88,8 +91,7 @@ history = classifier_model.fit(
 results = classifier_model.evaluate(test_ds, batch_size=batch_size)
 print("test loss, test acc:", results)
 
+# bert_raw_result = classifier_model(tf.constant(text_test))
+# print(tf.sigmoid(bert_raw_result))
 
-#bert_raw_result = classifier_model(tf.constant(text_test))
-#print(tf.sigmoid(bert_raw_result))
-
-#tf.keras.utils.plot_model(classifier_model)
+# tf.keras.utils.plot_model(classifier_model)
